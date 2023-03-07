fa4d3ed54dda852073f3236fcc899383f6412dac
Subanbek Tashbaltaev <stashbaltaev@gmail.com>
2023-03-08 00:38:14 +0600

    Completed task-5 (PP 2.2.1)

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,20 +16,28 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
-
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Benz", 10)));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Audi", 100)));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("MTZ", 12)));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Kaz", 13)));
+//
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println("Car = "+user.getCar());
+//         System.out.println();
+//      }
+      User user = userService.getUserWithCar("Benz", 10);
+      System.out.println("Id = "+user.getId());
+      System.out.println("First Name = "+user.getFirstName());
+      System.out.println("Last Name = "+user.getLastName());
+      System.out.println("Email = "+user.getEmail());
+      System.out.println("Car = "+user.getCar());
+      User user2 = userService.getUserWithCar("Benz", 11);
+      System.out.println(user2);
       context.close();
    }
 }
--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface UserService {
     void add(User user);
     List<User> listUsers();
+    User getUserWithCar(String model, int series);
 }
